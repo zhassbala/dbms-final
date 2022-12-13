37514c47f786a138f87cf138c1b7061cda263715
zhassbala <besimbekovr@gmail.com>
2022-12-14 04:58:02 +0600

    init

--- app/models.py
+++ app/models.py
@@ -13,6 +13,17 @@ class Hospital(models.Model):
     
     def __str__(self):
         return f'No. {self.hospital_no} {self.hospital_name}'
+    
+    def save(self, *args, **kwargs):
+        if not len(Administration.objects.filter(hospital=self)) > 0:
+            super().save(*args, **kwargs)
+            administration = Administration(hospital=self, department_name='Administration')
+            administration.save()
+        if not len(Economic.objects.filter(hospital=self)) > 0:
+            super().save(*args, **kwargs)
+            economic = Economic(hospital=self, department_name='Economic')
+            economic.save()
+        super().save(*args, **kwargs)
 
 
 class Department(models.Model):
@@ -23,7 +34,7 @@ class Department(models.Model):
     department_name = models.CharField(max_length=255)
     
     def __str__(self):
-        return self.department_name
+        return f'{self.department_name} department of {self.hospital}'
 
 
 class Employee(AbstractUser):
@@ -45,15 +56,22 @@ class Patient(models.Model):
     last_name = models.CharField(max_length=255)
     
     ID = models.CharField(primary_key=True, max_length=12, validators=[MinLengthValidator(12)])
+    
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
 
 
 class Salary(models.Model):
     employee = models.OneToOneField(to=Employee, on_delete=models.CASCADE)
     value = models.FloatField(validators=[MinValueValidator(0)])
+    
+    def __str__(self):
+        return f'{self.employee}\'s salary'
 
 
 class Doctor(Employee):
-    cabinet_number = models.IntegerField(unique=True)
+    cabinet_number = models.IntegerField()
+    rank = models.IntegerField(default=0)
     
     class Meta:
         verbose_name = 'Doctor'
@@ -61,11 +79,18 @@ class Doctor(Employee):
     def save(self, *args, **kwargs):
         try:
             calendar = self.calendar
-            super().save(*args, **kwargs)
         except: 
             super().save(*args, **kwargs)
             calendar = Calendar(doctor=self)
             calendar.save()
+        try:
+            self.salary.value = 250000+self.rank*8000
+            self.salary.save()
+        except: 
+            super().save(*args, **kwargs)
+            salary = Salary(employee=self, value=(250000+self.rank*8000))
+            salary.save()
+        super().save(*args, **kwargs)
 
 
 class Nurse(Employee):
@@ -79,12 +104,17 @@ class Nurse(Employee):
 class Calendar(models.Model):
     doctor = models.OneToOneField(
         to=Doctor, on_delete=models.CASCADE, primary_key=True)
+    
+    
+    def __str__(self):
+        return f'{self.doctor}\'s calendar'
 
 
 class Registration(models.Model):
-    calendar = models.ForeignKey(to=Calendar, on_delete=models.CASCADE)
+    calendar = models.ForeignKey(to=Calendar, on_delete=models.CASCADE, related_name='registrations')
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
+    patient = models.ForeignKey(to=Patient, on_delete=models.CASCADE, null=True)
 
     def save(self, *args, **kwargs):
         if self.start_time > self.end_time:
@@ -93,16 +123,21 @@ class Registration(models.Model):
         if not (9 <= self.start_time.hour < 18 and (9 <= self.end_time.hour < 18 or (self.end_time.hour == 18 and self.end_time.minute == 0))):
             raise AssertionError('Time range must be in the working time.')
 
-        calendar_set = self.calendar.calendar_items.filter(
+        calendar_set = self.calendar.registrations.filter(
             start_time__range=(self.start_time, self.end_time))
         if len(calendar_set) > 0:
-            raise AssertionError('This time is corrupted already.')
+            if not (len(calendar_set) == 1 and calendar_set.first() == self):
+                raise AssertionError('This time is corrupted already.')
 
-        calendar_set = self.calendar.calendar_items.filter(
+        calendar_set = self.calendar.registrations.filter(
             end_time__range=(self.start_time, self.end_time))
         if len(calendar_set) > 0:
-            raise AssertionError('This time is corrupted already.')
+            if not (len(calendar_set) == 1 and calendar_set.first() == self):
+                raise AssertionError('This time is corrupted already.')
         super().save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'Registration on {self.patient} {self.start_time} to {self.calendar.doctor}'
 
 
 class Examination(models.Model):
@@ -146,6 +181,11 @@ class Administration(Department):
     head_doctor = models.ForeignKey(
         to=Doctor, on_delete=models.SET_NULL, null=True, blank=True)
     department_name = 'Administration'
+    
+    def save(self, *args, **kwargs):
+        if (not self.head_doctor) and len(Doctor.objects.all()) > 0:
+            self.head_doctor = Doctor.objects.order_by('-rank')[0]
+        super().save(*args, **kwargs)
 
 
 class Economic(Department):
